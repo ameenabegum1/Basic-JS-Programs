30867d5df7b146343cd952ccf940876040ef4b72
ameenabegum1 <ameenabegum3212@gmail.com>
2026-05-04 14:53:45 +0530

    math.floor

--- stringrev.js
+++ stringrev.js
@@ -1,7 +1,8 @@
-let s='123' 
-let r=''
-for(let i=0;i<s.length;i++){
-    Math.floor(Math.random()*10)
-    r+=s[s.length-1-i]
+let s=123 
+let r=0
+for(let i=s;i>0;i=Math.floor(i/10)){    
+    r=r*10+i%10
+        
+    
 }
 console.log(r);
